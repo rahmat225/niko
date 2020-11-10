4381bd51d160643038b8cb53f86930caa9ae61ef
rahmat225 <74221925+rahmat225@users.noreply.github.com>
2020-11-10 18:47:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'NIKO BOT'; 
+const instagram = 'https://instagram.com/rhmteffal'; 
+const telegram = 'https://t.me/lusiapanjir'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
 //const grupch1 = 'belum ada grup'; 
